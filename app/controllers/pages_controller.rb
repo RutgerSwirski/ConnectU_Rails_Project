b1e1e6b1627084b05e90dd122e80760cd22584e3
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-04-18 15:04:25 +0100

    Added small fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @trips = Trip.order("date_leaving ASC").limit(9)
-    @activities = Activity.order("date ASC").limit(9)
-    @flats = Flat.order("RANDOM()").limit(9)
+    @trips = Trip.where("date_leaving > ?", Date.today).order("date_leaving ASC").limit(3)
+    @activities = Activity.where("date > ?", Date.today).order("date ASC").limit(3)
+    @flats = Flat.order("RANDOM()").limit(3)
   end
 end
