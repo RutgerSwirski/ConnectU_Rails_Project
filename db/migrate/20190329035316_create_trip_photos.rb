c1b7d5e48ba55cc7c8383e6cf4669d6507360781
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-04-24 14:53:13 +0100

    Updated Photo URL

--- db/migrate/20190329035316_create_trip_photos.rb
+++ db/migrate/20190329035316_create_trip_photos.rb
@@ -1,7 +1,7 @@
 class CreateTripPhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :trip_photos do |t|
-      t.string :photo, default: "eu7793sw3rerfbltachi.jpg"
+      t.string :photo, default: "dxwgaqkelr5kto0cpetf.jpg"
       t.references :trip, foreign_key: true
 
       t.timestamps
